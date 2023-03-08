c007d66890711bd718a26176a53e29f98537befe
Anna-K-S <anna.sergeevna1622@gmail.com>
2023-03-08 05:14:23 +0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: 100.0,
+                color: Colors.lightGreen,
+                child: const Text(''),
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.white,
+                  child: const Text('c2'),
+                ),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.grey,
+                  child: const Text('c2'),
+                )
+              ]),
+              Row(children: <Widget>[
+                Container(
+                  height: double.infinity,
+                  width: 100.0,
+                  color: Colors.red,
+                  child: const Text(''),
+                ),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
